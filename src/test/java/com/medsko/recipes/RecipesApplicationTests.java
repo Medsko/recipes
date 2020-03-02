ef7cbf47ecc87cdf7046818b3e1b18e62ea4b469
Medsko <medskodevries@gmail.com>
2020-03-02 14:34:30 +0100

    First commit

---
+++ src/test/java/com/medsko/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medsko.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
