ddbfa30a0b1cc2f1537cdc4ddfbe7b4e9f694ecf
Medsko <medskodevries@gmail.com>
2020-03-03 12:44:29 +0100

    Initialize data for (in memory) database

--- src/main/java/com/medsko/recipes/model/UnitOfMeasure.java
+++ src/main/java/com/medsko/recipes/model/UnitOfMeasure.java
@@ -12,7 +12,7 @@ public class UnitOfMeasure {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String uom;
+	private String description;
 
 	// TODO: provide alternate units of measurement depending on locale of user.
 
@@ -25,11 +25,11 @@ public class UnitOfMeasure {
 		this.id = id;
 	}
 
-	public String getUom() {
-		return uom;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setUom(String uom) {
-		this.uom = uom;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into Label (name) values ('American');
+insert into Label (name) values ('Italian');
+insert into Label (name) values ('Fast Food');
+insert into Label (name) values ('Mexican');
+insert into Label (name) values ('Dutch');
+insert into Unit_of_measure (description) values ('Tablespoon');
+insert into Unit_of_measure (description) values ('Teaspoon');
+insert into Unit_of_measure (description) values ('Gram');
+insert into Unit_of_measure (description) values ('Milliliter');
+insert into Unit_of_measure (description) values ('Liter');
+insert into Unit_of_measure (description) values ('Pinch');
+
